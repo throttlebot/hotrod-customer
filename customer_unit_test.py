ad2c48ccbb603d3f28e15b9ddf5deb995943dba0
Will Wang <willwang@Wills-MacBook-Pro.local>
2018-06-08 16:57:33 -0700

    can test now

---
+++ customer_unit_test.py
@@ -0,0 +1,18 @@
+import requests, time, sys
+from detect import detect
+
+command = "kubectl get service hotrod-frontend -o jsonpath='{.status.loadBalancer.ingress[0].ip}' -n "
+
+"""
+Checks that the server returns a valid response
+"""
+def test(host, port):
+	response = requests.get("http://" + host + ":" + str(port) + "/customer?customer=392")
+	assert response.status_code == 200, "Status code was {} instead of 200".format(response.status_code)
+	result = response.json()
+	assert result["Name"] == "Trom Chocolatier"
+	assert result["Location"] == "577,322"
+
+if __name__ == '__main__':
+
+	test("hotrod-customer", 8081)
